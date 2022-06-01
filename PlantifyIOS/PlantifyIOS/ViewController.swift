7514e0b8f9a5b8017ebd3d1ff75e7f68ed055f5f
Bruno <A01748931@tec.mx>
2022-05-31 21:35:08 -0500

    1er avance Carlos desde compu de Bruno

--- PlantifyIOS/PlantifyIOS/ViewController.swift
+++ PlantifyIOS/PlantifyIOS/ViewController.swift
@@ -12,9 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print("hello")
-        print("bruno commit")
-        print("Roberto Commit")
+        title = "Logout"
     }
 
 
