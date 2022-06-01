04b415b3145877e0d5ba0ab8a5480349dfdbe0b1
CarlosGarciaG <66147914+Hiler150620@users.noreply.github.com>
2022-05-31 19:15:25 -0500

    Primer commit

---
+++ PlantifyIOS/PlantifyIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlantifyIOS
+//
+//  Created by Carlos García Geronis on 31/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
